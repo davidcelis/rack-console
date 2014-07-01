8627cab264c0e28fbdd549236a8454182202f734
David Celis <me@davidcel.is>
2014-07-01 12:54:53 -0400

    Bump version to 1.1.0
    Signed-off-by: David Celis <me@davidcel.is>

--- lib/rack/console/version.rb
+++ lib/rack/console/version.rb
@@ -2,8 +2,8 @@ module Rack
   class Console
     class Version
       MAJOR = 1
-      MINOR = 0
-      PATCH = 1
+      MINOR = 1
+      PATCH = 0
 
       def self.to_s
         [MAJOR, MINOR, PATCH].join('.')
