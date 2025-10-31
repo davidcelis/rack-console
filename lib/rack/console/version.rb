edecd0ed1db5f682d08d10652fb17ec11ee8b3fc
David Celis <me@davidcel.is>
2025-10-31 16:51:09 -0700

    Bump version to 2.0.0

--- lib/rack/console/version.rb
+++ lib/rack/console/version.rb
@@ -1,9 +1,9 @@
 module Rack
   class Console
     class Version
-      MAJOR = 1
-      MINOR = 4
-      PATCH = 1
+      MAJOR = 2
+      MINOR = 0
+      PATCH = 0
 
       def self.to_s
         [MAJOR, MINOR, PATCH].join(".")
